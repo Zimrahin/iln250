d01c47a65c68c40abac50cc8b6ad8ce30cfe61b2
Mauricio <maurio.aravena@sansano.usm.cl>
2020-10-31 19:42:29 -0300

    Respaldo funcional, pero discreto

--- Tarea_1/tarea_1.c
+++ Tarea_1/tarea_1.c
@@ -4,38 +4,68 @@
 #include <math.h>
 // TEMP
 typedef struct set {
-	unsigned int* ArrayX;
-	unsigned int* ArrayY;
+	int* ArrayX;
+	int* ArrayY;
 	float* ArrayW;
-	unsigned int size;
-    unsigned int x0;
-    unsigned int y0;
+	int size;
+    float x0;
+    float y0;
 }Set;
 
+#define EPSILON 0.01
+#define KMAX 2
 //DECLARACIONES
-Set* getNewSet(unsigned int );
+Set* getNewSet( int );
 void generateDarray(Set* );
 void printValues(Set* );
 int randGenerator();
 void deleteArray(Set* );
 void initPoint(Set* );
 void printInit(Set* );
-
+// ESTAS SON NUEVAS
+void distance(Set* ptrSet, float* ptrDistances, int x0, int y0);
+void weiszfeld(Set* ptrSet, float epsilon,  int kmax, float Zk1);
+float sum(int i, Set* ptrSet, float* distances);
+void newPoint(Set* ptrSet, float* ptrDistances);
 
 /*----------------------------------------------------------------------------*/
 int main(int argc, char const *argv[]) {
 	printf("Hola mundo, soy la tarea de GIO! \n");
-	Set* test  = getNewSet(10);
+	//Set* test  = getNewSet(10);
+	int xs[] = {0, 0, 2, 2};
+	int ys[] = {0, 2, 0, 2};
+	float ws[] = {1.0, 1.0, 1.0, 1.0};
+	Set* test = malloc(sizeof(Set));
+	test->size = 4;
+	test->ArrayX = xs;
+	test->ArrayY = ys;
+	test->ArrayW = ws;
+	initPoint(test);
 	printValues(test);
 	printInit(test);
-	deleteArray(test);
+	weiszfeld(test, EPSILON, KMAX, 0);
+	printInit(test);
+	//deleteArray(test);
 	return 0;
 }
+/*------------------------------------------------------------*/
+// for (1 -- 100){}
+//     genrarset (10)
+//     for (1 3){
+//
+//         marca tiemo incio
+//         algoritmo i
+//         guardar en arreglo el tiempo
+//         marca tiempo final
+//     }
+//     freeset();
+// }
+// calcular promedio
 
 
 /*----------------------------------------------------------------------------*/
 /*funciones*/
-Set* getNewSet(unsigned int size){
+Set* getNewSet( int size){
     Set* newSet = NULL;
     if(size > 0){
         newSet = (Set*)malloc(sizeof(Set));
@@ -47,18 +77,19 @@ Set* getNewSet(unsigned int size){
 
 }
 
-void generateDarray(Set* ptrSet){
-	unsigned int size = ptrSet->size;
-	unsigned int* arrTempX = malloc(sizeof(unsigned int)*size);
-	unsigned int* arrTempY = malloc(sizeof(unsigned int)*size);
+void generateDarray(Set* ptrSet) {
+	int size = ptrSet->size;
+	int* arrTempX = malloc(sizeof( int)*size);
+	int* arrTempY = malloc(sizeof( int)*size);
 	float* arrTempW = malloc(sizeof(float)*size);
-    srand(time(0));
-	for(unsigned int i = 0; i < size; i++){
-		arrTempX[i] = rand() % (size+1);
-		arrTempY[i] = rand() % (size+1);
+    srand48(time(0));
+	for( int i = 0; i < size; i++){
+		arrTempX[i] = lrand48() % (size+1);
+		arrTempY[i] = lrand48() % (size+1);
+		//The lrand48() function returns non-negative, long integers, uniformly distributed over the interval [0,RAND_MAX].
         int lower = size/10;
         int upper = lower*5;
-		arrTempW[i] = ((float)rand()/(float)(RAND_MAX))*(upper-lower) + lower;
+		arrTempW[i] = ((float)lrand48()/(float)(RAND_MAX))*(upper-lower) + lower;
 	}
 	ptrSet->ArrayX = arrTempX;
 	ptrSet->ArrayY = arrTempY;
@@ -66,9 +97,9 @@ void generateDarray(Set* ptrSet){
 	initPoint(ptrSet);
 }
 
-void printValues(Set* ptrSet){
-	unsigned int size = ptrSet->size;
-	for(unsigned int i = 0; i < size; i++){
+void printValues(Set* ptrSet) {
+	int size = ptrSet->size;
+	for(int i = 0; i < size; i++){
 		printf("[%u] Valores Xi = %u, Yi = %u, Wi = %f\n", (i+1),ptrSet->ArrayX[i],ptrSet->ArrayY[i], ptrSet->ArrayW[i]);
 	}
 }
@@ -79,16 +110,15 @@ void deleteArray(Set* ptrSet){
 	free(ptrSet->ArrayW);
 	free(ptrSet);
 }
-
-void initPoint(Set* ptrSet){
-	unsigned int size = ptrSet->size;
+void initPoint(Set* ptrSet) {
+	int size = ptrSet->size;
 	float tempSumWX = 0;
 	float tempSumWY = 0;
 	float tempSumW = 0;
-	unsigned int tempX  = 0;
-	unsigned int tempY  = 0;
+	int tempX  = 0;
+	int tempY  = 0;
 	float tempW = 0;
-	for(unsigned int i = 0; i < size; i++){
+	for(int i = 0; i < size; i++){
 		tempX  = ptrSet->ArrayX[i];
 		tempY  = ptrSet->ArrayY[i];
 		tempW = ptrSet->ArrayW[i];
@@ -104,16 +134,57 @@ void printInit(Set* ptrSet){
 	printf("Soluciones iniciales Xo = %u, Yo = %u\n", ptrSet->x0, ptrSet->y0);
 }
 
-/*------------*/
-// for (1 -- 100){}
-//     genrarset (10)
-//     for (1 3){
-//
-//         marca tiemo incio
-//         algoritmo i
-//         guardar en arreglo el tiempo
-//         marca tiempo final
-//     }
-//     freeset();
-// }
-// calcular promedio
+void weiszfeld(Set* ptrSet, float epsilon,  int kmax, float Zk1) {
+	float* distances = malloc(sizeof(float)*(ptrSet->size));
+	distance(ptrSet, distances, ptrSet->x0, ptrSet->y0);
+
+	float Zk = sum(ptrSet->size, ptrSet, distances);
+	//Esto se ejecuta la primera vez
+	newPoint(ptrSet, distances); // Seteo de nuevo punto inicial
+	free(distances);
+	if ( (kmax <= 0) || abs(Zk - Zk1) < epsilon ){
+		//Terminar y guardar
+		printf("Termine ! \n");
+	}
+	else{
+		weiszfeld(ptrSet, epsilon, kmax-1, Zk);
+	}
+}
+
+void distance(Set* ptrSet, float* ptrDistances, int x0, int y0) {
+	 int* Xs = ptrSet->ArrayX;
+	 int* Ys = ptrSet->ArrayY;
+	for(int i = 0; i < ptrSet->size; i++) {
+		ptrDistances[i] = pow(Xs[i]-x0,2) + pow(Ys[i]-y0,2);
+		ptrDistances[i] = sqrt(ptrDistances[i]);
+	}
+}
+
+float sum(int i, Set* ptrSet, float* distances) {
+	if(i == 1) {
+		return ( (ptrSet->ArrayW[0]) * distances[0] );
+	}
+	return sum(i-1, ptrSet, distances) + (ptrSet->ArrayW[i-1]) * distances[i-1];
+}
+
+void newPoint(Set* ptrSet, float* ptrDistances) {
+	int size = ptrSet->size;
+	float tempSumWX = 0;
+	float tempSumWY = 0;
+	float tempSumW = 0;
+	int tempX  = 0;
+	int tempY  = 0;
+	float tempD = 0;
+	float tempW = 0;
+	for(int i = 0; i < size; i++){
+		tempX  = ptrSet->ArrayX[i];
+		tempY  = ptrSet->ArrayY[i];
+		tempD  = ptrDistances[i];
+		tempW  = ptrSet->ArrayW[i];
+		tempSumWX += tempW*tempX/tempD;
+		tempSumWY += tempW*tempY/tempD;
+		tempSumW  += tempW/tempD;
+	}
+	ptrSet->x0 = round(tempSumWX/tempSumW);
+    ptrSet->y0 = round(tempSumWY/tempSumW);
+}
